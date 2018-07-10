cd3d8eff56f9812349168e8b6339557912d4aaaa
jonh1036 <jonh1036@hotmail.com>
2018-07-10 12:51:41 -0300

    Processing cases with Switch

--- main.c
+++ main.c
@@ -7,6 +7,27 @@ int x = 0, y = 0;//Coordenadas da matriz
 char mat[10][10];//Criação da matriz
 
 int main(void) {
+    char tecla;
+    
+    while(1){
+		tecla = getch();
+		system("cls");
+		switch(tecla){
+		    case 97: //Movimentar para esquerda
+		        break;
+		    case 100: //Movimentar para direita
+		        break;
+			case 113: //Para encerrar o jogo
+		      	
+			case 115: //Movimentar para baixo
+		    	break;
+		    case 119: //Movimentar para cima
+		        break;
+	        default: 
+	            break;
+        }
 	
+    }
+    printf("\n\n");	
 	return 0;
 }
