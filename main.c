94fdd8f8fe6e6ccadb34ae093ea001902277756c
jonh1036 <jonh1036@hotmail.com>
2018-07-10 12:46:50 -0300

    Creation of matrix 10X10

---
+++ main.c
@@ -0,0 +1,12 @@
+//  Created by Jonathan Pinho 10/07/2018.
+//  Copyright © 2018 Jonathan. All rights reserved.
+
+#include <stdio.h>
+
+int x = 0, y = 0;//Coordenadas da matriz
+char mat[10][10];//Criação da matriz
+
+int main(void) {
+	
+	return 0;
+}
