4e3cd470209f94f066841799701d04d07931446b
jonh1036 <jonh1036@hotmail.com>
2018-09-09 14:42:48 -0300

    Deleting functions useless

--- snake.c
+++ snake.c
@@ -17,39 +17,30 @@ typedef struct  {
     int life;
 } Candy;
 
-void inicializa();
-void imprimir();
-void gerarDoce();
-void movimentar();
-void direita();
-void esquerda();
-void cima();
-void baixo();
-void rigth();
-void left();
-void up();
-void down();
-int ponteiroNulo(Position *p);
-void insert();
-void clear();
 void aux(Position p);
+void clear();
+void gerarDoce();
+void imprimir();
+void inicializa();
+void insert();
+void movimentar();
+int ponteiroNulo(Position *p);
 
-int x = 2, y = 0;//Coordenadas da matriz
 char mat[MAT][MAT];//Criação da matriz
-char tecla = 'd';
-Snake snake;
-Candy candy;
+char tecla = 'd';//Tecla de início de jogo
+Snake snake;//Variável global para a cobra
+Candy candy;//Variável global para o doce
 
 int main(void) {
     
-	inicializa();//Inicialização da Matriz
+	inicializa();
 	movimentar();
 	
     printf("\n\n");	
 	return 0;
 }
 
-void inicializa(){//Função que inicializa a matriz no início do programa
+void inicializa(){//Inicializa a matriz no início do programa
     int i;
 	
 	snake.size = 3; 
@@ -66,7 +57,83 @@ void inicializa(){//Função que inicializa a matriz no início do programa
     insert();
 }
 
-void clear(){
+void movimentar(){//mover
+	char tec;
+	int i;
+	
+	Position head = snake.p[0];
+	while(1){
+		imprimir();
+		
+		if (kbhit()){
+			tec = getch();
+			if( (tecla=='a' && tec != 'd') || (tecla=='d' && tec != 'a') || (tecla=='w' && tec != 's') || (tecla=='s' && tec != 'w') ){
+				tecla = tec;	
+			}
+		}
+		switch(tecla){
+	    	case 'a':	head.y--;
+	    				candy.life--;
+	    	    break;
+	    	case 'd':	head.y++;
+						candy.life--;
+	    	    break;
+			case 'q':	exit(0);//Para encerrar o jogo
+	    	  	
+			case 's':	head.x++;
+						candy.life--;
+	    		break;
+	    	case 'w':	head.x--;
+						candy.life--;
+	    	    break;
+	    	default:	printf("Teclas permitidas: \nPara cima (tecla W), \nPara baixo (tecla S),\nPara a esquerda (tecla A)\nPara a direita (tecla D)\nPara sair (tecla Q)\n\n");
+	    	    break;
+		}
+		if(candy.life == 0){//Caso a vida do doce acabe, será criado outro doce em outra posição aleatória na matriz
+    		mat[candy.position.y][candy.position.x] = ' ';
+    		gerarDoce();
+		}
+	
+		if(head.x == candy.position.x  &&  head.y == candy.position.y ){//Caso de teste para a colisão da cobra com um doce
+    	   	snake.size++;
+    		gerarDoce();
+    	}
+		
+		if(head.x == 10 || head.x == -1 || head.y == 10 || head.y == -1){//Caso de teste para verificar se a cobra bateu em alguma extremidade da matriz
+			puts("\nGame Over");
+			free(snake.p);
+			exit(0);
+		}
+	
+		for(i = 1; i < snake.size; i++){//Verifica se bateu no próprio corpo
+    	    if(snake.p[0].x == snake.p[i].x  &&  snake.p[0].y == snake.p[i].y){
+    	        puts("\nGame Over");
+				free(snake.p);
+				exit(0);
+    	    }
+    	}
+		
+		aux(head);
+		clear();
+		insert();
+		system("cls");
+	}
+}
+
+void aux(Position p){
+	int i;
+	
+	snake.p = (Position *) realloc( snake.p, snake.size * sizeof(Position));//Realoca a memória para aumentar o corpo da cobra
+	for(i = snake.size -1; i >= 0 ;i--){
+        if(i == 0){
+            snake.p[i] = p;
+        }else{
+            snake.p[i] = snake.p[i - 1] ;
+        }
+    }
+}
+
+void clear(){//Limpa a matriz
     int i, j;
 	for(i = 0; i < MAT ; i++)
         for(j = 0; j < MAT ; j++){
@@ -74,7 +141,7 @@ void clear(){
         }    
 }
 
-void imprimir(){//Função que imprime a matriz completa
+void imprimir(){//Imprime a matriz
     int i, j;
     for(i = 0; i < 10; i++){
         printf("|");
@@ -85,8 +152,8 @@ void imprimir(){//Função que imprime a matriz completa
     }
 }
 
-void gerarDoce(){
-	int x1, y1,i;
+void gerarDoce(){//Gera um doce com coordenada aleatória
+	int i;
 	
 	do{
 	   	srand(time(NULL));
@@ -100,10 +167,10 @@ void gerarDoce(){
 		}
 	}while(1);
 	
-	candy.life = rand()%10;
+	candy.life = rand()%10;//Define a vida do doce
 }
 
-void insert(){
+void insert(){//Cadastra o corpo da cobra na matriz
 	int i;
 	
 	for(i = 0; i < snake.size; i++) {
@@ -111,208 +178,10 @@ void insert(){
 	}
 }
 
-int ponteiroNulo (Position *p) {
+int ponteiroNulo (Position *p){//Retorna verdadeiro ou falso para a alocação de memória
 	if(p == NULL) {
 		printf("Does not possible alloc");
 		exit(1);
 	}
 	return 0;
-}
-
-void movimentar(){//mover
-	char tec;
-	
-	Position head = snake.p[0];
-	system("cls");
-	while(1){
-		imprimir();
-		
-		if (kbhit()){
-			tec = getch();
-			if( (tecla=='a' && tec != 'd') || (tecla=='d' && tec != 'a') || (tecla=='w' && tec != 's') || (tecla=='s' && tec != 'w') ){
-				tecla = tec;	
-			}
-		}
-		switch(tecla){
-	    	case 'a':	//esquerda();//Movimentar para esquerda
-	    				head.y--;
-	    				candy.life--;
-	    	    break;
-	    	case 'd':	//direita();//Movimentar para direita
-	    				head.y++;
-						candy.life--;
-	    	    break;
-			case 'q':	exit(0);//Para encerrar o jogo
-	    	  	
-			case 's':	//baixo();//Movimentar para baixo
-						head.x++;
-						candy.life--;
-	    		break;
-	    	case 'w':	//cima();//Movimentar para cima
-	    				head.x--;
-						candy.life--;
-	    	    break;
-	    	default:	printf("Teclas permitidas: \nPara cima (tecla W), \nPara baixo (tecla S),\nPara a esquerda (tecla A)\nPara a direita (tecla D)\nPara sair (tecla Q)\n\n");
-	    	    break;
-		}
-		if(candy.life == 0){
-    		mat[candy.position.y][candy.position.x] = ' ';
-    		gerarDoce();
-		}
-	
-		if(head.x == candy.position.x  &&  head.y == candy.position.y ){
-    	   	snake.size++;
-    		gerarDoce();
-    	}
-	
-		aux(head);
-		clear();
-		insert();
-		system("cls");
-	}
-}
-
-void aux(Position p){
-	int i;
-	
-	snake.p = (Position *) realloc( snake.p, snake.size * sizeof(Position));
-	for(i = snake.size -1; i >= 0 ;i--){
-        if(i == 0){
-            snake.p[i] = p;
-        }else{
-            snake.p[i] = snake.p[i - 1] ;
-        }
-    }
-}
-
-void direita(){//Função que move o '*' para a direita
-    char aux;
-    int i;
-
-	if((x+1)<10 && mat[y][x+1] == '$') { //Caso de teste quando se encontra o doce
-		mat[y][x+1] = '*';
-		
-		snake.size++;
-	
-		int x1, y1;
-		do{
-		  	gerarDoce();
-			break;
-		}while(1);
-		rigth();	
-	}
-	else if(((x+1)<10 && mat[y][x+1] == '*') || ((x+1)==10)){ //Caso de teste quando se encontra o outra parte do corpo ou com a parede
-		puts("GAMER OVER");
-		exit(0);
-	}
-	else if((x+1)<10) { //Adicionando limite para a extremidade direita
-	    rigth();
-	}
-}
-
-void rigth(){
-	char aux;
-	aux = mat[y][x];
-	mat[y][x] = mat[y][x+1];
-	mat[y][x+1] = aux;
-	x +=1;	
-}
-
-void esquerda(){//Função que move o '*' para a esquerda
-    char aux;
-
-	if((x-1)>0 && mat[y][x-1] == '$') { //Caso de teste quando se encontra o doce
-		mat[y][x-1] = '*';
-		
-		snake.size++;
-		
-		int x1, y1,i;
-		do{
-		   	gerarDoce();
-			break;
-		}while(1);
-		left();
-	}
-	else if(((x-1)>0 && mat[y][x-1] == '*') || ((x-1)<0)){ //Caso de teste quando se encontra o outra parte do corpo ou com a parede
-		puts("GAMER OVER");
-		exit(0);
-	}
-	else if((x-1)>=0) { //Adicionando limite para a extremidade esquerda
-	    left();
-	}
-}
-
-void left(){
-	char aux;
-	
-	aux = mat[y][x];
-	mat[y][x] = mat[y][x-1];
-	mat[y][x-1] = aux;
-	x -=1;
-}
-
-void cima(){//Função que move o '*' para a cima
-    char aux;
-
-	if((y-1)>0 && mat[y-1][x] == '$') { //Caso de teste quando se encontra o doce
-		mat[y-1][x] = '*';
-		
-		snake.size++;
-		
-		int x1, y1,i;
-		do{
-		   	gerarDoce();
-			break;
-		}while(1);	
-		up();
-	}
-	else if(((y-1)>0 && mat[y-1][x] == '*') || ((y-1)<0)){ //Caso de teste quando se encontra o outra parte do corpo ou com a parede
-		puts("GAMER OVER");
-		exit(0);
-	}
-	else if((y-1)>=0) { //Adicionando limite para a extremidade de cima
-		up();
-	}
-}
-
-void up(){
-	char aux;
-	
-	aux = mat[y][x];
-	mat[y][x] = mat[y-1][x];
-	mat[y-1][x] = aux;
-	y -=1;
-}
-
-void baixo(){//Função que move o '*' para a baixo
-    char aux;
-
-	if((y+1)<10 && mat[y+1][x] == '$') { //Caso de teste quando se encontra o doce
-		mat[y+1][x] = '*';
-		
-		snake.size++;
-		
-		int x1, y1,i;
-		do{
-			gerarDoce();
-			break;
-		}while(1);
-		down();
-	}
-	else if(((y+1)<10 && mat[y+1][x] == '*') || ((y+1)==10)){ //Caso de teste quando se encontra o outra parte do corpo ou com a parede
-		puts("GAMER OVER");
-		exit(0);
-	}
-	else if((y+1)<10) { //Adicionando limite para a extremidade de baixo
-	    down();
-	}  
-}
-
-void down(){
-	char aux;
-	
-	aux = mat[y][x];
-	mat[y][x] = mat[y+1][x];
-	mat[y+1][x] = aux;
-	y +=1;
 }
