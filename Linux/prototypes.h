0373da1ade4ca2a0d9d912889cebf32a3c8be224
jonh1036 <jonh1036@hotmail.com>
2018-09-12 15:15:51 -0300

    Initial code for Linux

---
+++ Linux/prototypes.h
@@ -0,0 +1,23 @@
+#define MAT 10
+#define MAX_WORM 100
+
+typedef struct  {
+    int x,y;
+}Position;
+typedef struct  {
+    int size;
+    Position *p;
+}Snake;
+typedef struct  {
+    Position position;
+    int life;
+}Candy;
+
+void aux(Position p);
+void clear();
+void gerarDoce();
+void imprimir();
+void inicializa();
+void insert();
+void movimentar();
+int ponteiroNulo(Position *p);
---
+++ Linux/snake.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include "functions.c"
+
+int main(void) {
+	inicializa();
+	movimentar();
+    printf("\n\n");	
+	return 0;
+}
